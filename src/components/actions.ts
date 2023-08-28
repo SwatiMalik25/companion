c984e9002de26d3dcae1e067d00bd4af4d801c19
SwatiMalik25 <anna123456hathaway@gmail.com>
2023-08-28 20:33:03 +0530

    new

--- src/components/actions.ts
+++ src/components/actions.ts
@@ -1,17 +1,12 @@
 "use server";
 
-// server action to allow configuration of LLM from .env.local
-
-import dotenv from "dotenv";
-import { parse } from "path";
-
 
 export async function getCompanions() {
-  const COMPFILE = "companions\companions.json";
+  const id = "./companions/companions.json";
   var companions = [];
   // console.log("Loading companion descriptions from "+COMPFILE);
   var fs = require('fs');
-  const data = fs.readFileSync(COMPFILE);
+  const data = fs.readFileSync(id);
   console.log(String(data));
   // run a parse here to force a server side error if the JSON is improperly formatted
   // It's much more difficult to debug client side
