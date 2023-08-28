a3dbbe36499d661473eedc7482f0df5c689c777b
SwatiMalik25 <anna123456hathaway@gmail.com>
2023-08-28 11:47:35 +0530

    new

--- next.config.js
+++ next.config.js
@@ -42,9 +42,11 @@ const nextConfig = {
   },
 };
 
-export function webpack(config, { isServer }) {
-  if (!isServer) {
-    config.resolve.fallback.fs = false;
-  }
-  return config;
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
 }
