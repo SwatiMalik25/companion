1d35ecaaa719f552adf2332e737de0c753d6ef8f
SwatiMalik25 <anna123456hathaway@gmail.com>
2023-08-28 12:16:47 +0530

    new

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-  experimental: {
+  
     serverActions: true,
+    output: 'export',
 
-  },
+
   trailingSlash: true,
   images: {
     remotePatterns: [
